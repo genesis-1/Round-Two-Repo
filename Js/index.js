3553ca7801a2fe4ffed2fa0997533ab7468847ea
patrick ntwali <patrick_ntwari@yahoo.com>
2021-11-12 11:59:12 +0200

    PNT: added decount incount logics

--- Js/index.js
+++ Js/index.js
@@ -1,7 +1,53 @@
-function count()
+
+
+
+var constMinusLimit = 0;
+var constPlusLimit = 0;
+var isPlusCountOver = false;
+var isMinCountOver = false;
+
+function Decount()
 {
-    var value = parseInt(document.getElementById('number').value, 10);
-    value = isNaN(value) ? 0 : value;
-    value++;
-    document.getElementById('number').value = value;
+    var limit = 0;
+    var pluseValue = parseInt(document.getElementById('plusInput').value, 10);
+
+    var minsValue = parseInt(document.getElementById('minInput').value, 10);
+
+    limit = pluseValue + minsValue;
+    if(constMinusLimit == 0 && isMinCountOver != true){
+        constMinusLimit = limit;
+    }
+
+    if(constMinusLimit >= 0){
+        
+        document.getElementById('number').value = constMinusLimit;
+        constMinusLimit = constMinusLimit - 1;
+        if(constMinusLimit == 0){
+            isMinCountOver = true;
+        }
+    }
 }
+
+
+function Incount()
+{
+
+    var limit = 0;
+    var pluseValue = parseInt(document.getElementById('plusInput').value, 10);
+
+    var minsValue = parseInt(document.getElementById('minInput').value, 10);
+
+    limit = pluseValue + minsValue;
+    if(constPlusLimit == 0 && isPlusCountOver != true){
+        constPlusLimit = 0;
+    }
+
+    if(constPlusLimit <= limit){
+        document.getElementById('number').value = constPlusLimit;
+        constPlusLimit = constPlusLimit + 1;
+        if(constPlusLimit == limit){
+            isMinCountOver = true;
+        }
+    }
+}
+
